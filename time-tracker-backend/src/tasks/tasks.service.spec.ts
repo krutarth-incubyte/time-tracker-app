4c99b09f89f64efdc811c7f05f6e20ff4eaa4b50
Krutarth Dave <krutarth@incubyte.co>
2025-07-10 16:09:19 +0530

    BE: add failing test for findAll method

--- time-tracker-backend/src/tasks/tasks.service.spec.ts
+++ time-tracker-backend/src/tasks/tasks.service.spec.ts
@@ -5,22 +5,22 @@ import { CreateTaskDto } from './dto/create-task.dto';
 import { TaskStatus } from './entities/task.entity';
 
 describe('TasksService', () => {
-  let service: TasksService;
+  let taskService: TasksService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [TasksService],
     }).compile();
 
-    service = module.get<TasksService>(TasksService);
+    taskService = module.get<TasksService>(TasksService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(taskService).toBeDefined();
   });
 
   it('should create a task', () => {
-    const task = service.create({
+    const task = taskService.create({
       title: 'Test Task',
       description: 'Test Description',
     } as unknown as CreateTaskDto);
@@ -32,5 +32,38 @@ describe('TasksService', () => {
       createdAt: expect.any(Date),
       status: TaskStatus.PENDING,
     });
+  });
+
+  it('should return all tasks', () => {
+    // Create some tasks first
+    taskService.create({
+      title: 'Test Task',
+      description: 'Test Description',
+    } as unknown as CreateTaskDto);
+
+    taskService.create({
+      title: 'Test Task 2',
+      description: 'Test Description 2',
+    } as unknown as CreateTaskDto);
+
+    // Now test findAll
+    const tasks = taskService.findAll();
+    expect(tasks).toHaveLength(2);
+    expect(tasks).toEqual([
+      {
+        id: expect.any(Number),
+        title: 'Test Task',
+        description: 'Test Description',
+        createdAt: expect.any(Date),
+        status: TaskStatus.PENDING,
+      },
+      {
+        id: expect.any(Number),
+        title: 'Test Task 2',
+        description: 'Test Description 2',
+        createdAt: expect.any(Date),
+        status: TaskStatus.PENDING,
+      },
+    ]);
   });
 });
