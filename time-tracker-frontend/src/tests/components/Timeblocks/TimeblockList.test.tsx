be350fe52e021b7f66713122bcd8cef6a3eb3367
Krutarth Dave <krutarth@incubyte.co>
2025-07-15 17:23:18 +0530

    FE: Add failing test case to render timeblock-list with data

--- time-tracker-frontend/src/tests/components/Timeblocks/TimeblockList.test.tsx
+++ time-tracker-frontend/src/tests/components/Timeblocks/TimeblockList.test.tsx
@@ -1,9 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import TimeblockList from "@/components/Timeblocks/TimeblockList";
+import { tasks as mockTasks } from "@/components/Tasks/mockTasks";
 
 describe("TimeblockList", () => {
   it("should render timeblock list", () => {
     render(<TimeblockList />);
-    expect(screen.getByText("Timeblock List")).toBeInTheDocument();
+    expect(screen.getByText(/Timeblock List/i)).toBeInTheDocument();
+  });
+
+  it("should render timeblock list with timeblocks", () => {
+    render(<TimeblockList timeblocks={mockTasks[0].timeBlocks} />);
+    expect(screen.getByText(/Timeblock 1 description/i)).toBeInTheDocument();
   });
 });
