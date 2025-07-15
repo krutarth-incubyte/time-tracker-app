8d5272a5f4ef31fd3d17335da9e82ddcb07f581f
Krutarth Dave <krutarth@incubyte.co>
2025-07-15 12:37:21 +0530

    FE: Add failing test case for add task dialog

---
+++ time-tracker-frontend/src/tests/components/Tasks/AddTask.test.tsx
@@ -0,0 +1,19 @@
+import { describe, it, expect } from "vitest";
+
+import { render, screen, fireEvent } from "@testing-library/react";
+import AddTask from "@/components/Tasks/AddTask";
+
+describe("AddTask", () => {
+  const AddButtonText = /Add Task/i;
+  const DialogTitleText = /Add A Task/i;
+  it("should render add task button", () => {
+    render(<AddTask />);
+    expect(screen.getByText(AddButtonText)).toBeInTheDocument();
+  });
+
+  it("should open dialog when add task button is clicked", () => {
+    render(<AddTask />);
+    fireEvent.click(screen.getByText(AddButtonText));
+    expect(screen.getByText(DialogTitleText)).toBeInTheDocument();
+  });
+});
