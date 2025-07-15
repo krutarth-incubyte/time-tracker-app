2b38cf2455aad348e2856db4f51de2e806622ff6
Krutarth Dave <krutarth@incubyte.co>
2025-07-15 11:52:34 +0530

    FE: Add code to pass the test

--- time-tracker-frontend/src/components/Tasks/TaskList.tsx
+++ time-tracker-frontend/src/components/Tasks/TaskList.tsx
@@ -1,11 +1,21 @@
 import type { Task } from "../../types/tasks";
 
-export default function TaskList({ tasks }: { tasks?: Task[] }) {
+export default function TaskList({ tasks = [] }: { tasks: Task[] }) {
   return (
-    <div>
-      {tasks?.map((task) => (
-        <div key={task.id}>{task.title}</div>
-      ))}
+    // TODO: make it a grid
+    <div className="flex gap-2">
+      {tasks.length > 0 ? (
+        tasks.map((task) => (
+          <div key={task.id} className="flex flex-col gap-2">
+            <h2>{task.title}</h2>
+            <p>{task.description}</p>
+            <p>{task.createdAt.toLocaleDateString()}</p>
+            <p>{task.status}</p>
+          </div>
+        ))
+      ) : (
+        <div>No tasks found</div>
+      )}
     </div>
   );
 }
