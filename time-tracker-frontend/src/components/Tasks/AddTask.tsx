234a15796c4921acb0973267412a54bace40f64b
Krutarth Dave <krutarth@incubyte.co>
2025-07-15 13:03:47 +0530

    FE: Add code to pass the failing test for task input form

--- time-tracker-frontend/src/components/Tasks/AddTask.tsx
+++ time-tracker-frontend/src/components/Tasks/AddTask.tsx
@@ -9,6 +9,7 @@ import {
   DialogTrigger,
 } from "../ui/dialog";
 import { XIcon } from "lucide-react";
+import { Input } from "../ui/input";
 
 export default function AddTask() {
   const [open, setOpen] = useState(false);
@@ -18,19 +19,31 @@ export default function AddTask() {
         <Button>Add Task</Button>
       </DialogTrigger>
       <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Add A Task</DialogTitle>
-          <DialogClose asChild>
-            <Button
-              variant="outline"
-              size="icon"
-              className="absolute top-2 right-2"
-              data-testid="close-button"
-            >
-              <XIcon className="w-4 h-4" stroke="white" />
-            </Button>
-          </DialogClose>
-        </DialogHeader>
+        <form>
+          <DialogHeader>
+            <DialogTitle>Add A Task</DialogTitle>
+            <DialogClose asChild>
+              <Button
+                variant="outline"
+                size="icon"
+                className="absolute top-2 right-2"
+                data-testid="close-button"
+              >
+                <XIcon className="w-4 h-4" stroke="white" />
+              </Button>
+            </DialogClose>
+          </DialogHeader>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="task-name">Task Name</label>
+            <Input type="text" placeholder="Task Name" id="task-name" />
+            <label htmlFor="task-description">Task Description</label>
+            <Input
+              type="text"
+              placeholder="Task Description"
+              id="task-description"
+            />
+          </div>
+        </form>
       </DialogContent>
     </Dialog>
   );
